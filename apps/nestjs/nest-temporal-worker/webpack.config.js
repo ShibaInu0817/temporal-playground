e27c0f5228ff42ace88b60bf05ce75a98afd1e51
Tee Tian Xiang <txtee@TXs-MacBook-Pro.local>
2025-02-08 12:01:14 +0800

    Recreate 2 temporal worker app

--- apps/nestjs/nest-temporal-worker/webpack.config.js
+++ apps/nestjs/nest-temporal-worker/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/nestjs-app'),
+    path: join(__dirname, '../../../dist/apps/nestjs/nest-temporal-worker'),
   },
   plugins: [
     new NxAppWebpackPlugin({
